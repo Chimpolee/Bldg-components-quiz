053e3659e25e2de4b927f1e61801e6078f7483d7
Eromosele Prosper <109613638+Chimpolee@users.noreply.github.com>
2023-03-03 20:45:08 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,32 @@
+const correctAnswers = ['D', 'B', 'D', 'A', 'D'];
+const form = document.querySelector('.quiz-form');
+const scores = document.querySelector('.score-board');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value,];
+    //correct answers
+    userAnswers.forEach((answer, index) =>{
+        if(answer === correctAnswers[index]){
+            score += 20;
+        }
+    });
+    console.log(score);
+ 
+    scrollTo(0, 0);
+    scores.querySelector('span').textContent = `${score}%`;
+    scores.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() =>{
+        scores.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 30);
+});
+
